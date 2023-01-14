300ec540270e928ca11d517b67448406e5852219
Hrafn Orri Hrafnkelsson <HrafnOrri@BitCrow.net>
2023-01-14 18:04:03 +0000

    crate docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,169 @@
+#![warn(missing_docs)]
+//! # Rust Sprite Animator
+//!
+//! A basic state machine for managing sprite animations.
+//!
+//! Example usage:
+//!
+//! ```
+//! use rsanim::*;
+//! use std::collections::HashMap;
+//!
+//! #[derive(Clone, Eq, PartialEq, Hash, Debug)]
+//! enum Animation {
+//!     Idle,
+//!     Run,
+//! }
+//!
+//! #[derive(Clone, Debug, PartialEq)]
+//! struct Params {
+//!     pub speed: f32,
+//! }
+//!
+//! let mut state_machine = StateMachine::new(
+//!     Animation::Idle,
+//!     HashMap::from([
+//!         (
+//!             Animation::Idle,
+//!             State {
+//!                 duration: 0.5,
+//!                 repeat: true,
+//!             },
+//!         ),
+//!         (
+//!             Animation::Run,
+//!             State {
+//!                 duration: 1.0,
+//!                 repeat: true,
+//!             },
+//!         ),
+//!     ]),
+//!     vec![
+//!         Transition {
+//!             start_state: TransitionStartState::Node(Animation::Idle),
+//!             end_state: TransitionEndState::Node(Animation::Run),
+//!             trigger: Trigger::Condition(Box::new(|x: &Params| x.speed > 0.0)),
+//!         },
+//!         Transition {
+//!             start_state: TransitionStartState::Node(Animation::Run),
+//!             end_state: TransitionEndState::Node(Animation::Idle),
+//!             trigger: Trigger::Condition(Box::new(|x: &Params| x.speed <= 0.0)),
+//!         },
+//!     ],
+//!     Params { speed: 0.0 },
+//! )
+//! .unwrap();
+//! ```
+//!
+//! Update the state machine as time passes:
+//!
+//! ```
+//! # use rsanim::*;
+//! # use std::collections::HashMap;
+//!
+//! # #[derive(Clone, Eq, PartialEq, Hash, Debug)]
+//! # enum Animation {
+//! #     Idle,
+//! #     Run,
+//! # }
+//!
+//! # #[derive(Clone, Debug, PartialEq)]
+//! # struct Params {
+//! #     pub speed: f32,
+//! # }
+//!
+//! # let mut state_machine = StateMachine::new(
+//! #     Animation::Idle,
+//! #     HashMap::from([
+//! #         (
+//! #             Animation::Idle,
+//! #             State {
+//! #                 duration: 0.5,
+//! #                 repeat: true,
+//! #             },
+//! #         ),
+//! #         (
+//! #             Animation::Run,
+//! #             State {
+//! #                 duration: 1.0,
+//! #                 repeat: true,
+//! #             },
+//! #         ),
+//! #     ]),
+//! #     vec![
+//! #         Transition {
+//! #             start_state: TransitionStartState::Node(Animation::Idle),
+//! #             end_state: TransitionEndState::Node(Animation::Run),
+//! #             trigger: Trigger::Condition(Box::new(|x: &Params| x.speed > 0.0)),
+//! #         },
+//! #         Transition {
+//! #             start_state: TransitionStartState::Node(Animation::Run),
+//! #             end_state: TransitionEndState::Node(Animation::Idle),
+//! #             trigger: Trigger::Condition(Box::new(|x: &Params| x.speed <= 0.0)),
+//! #         },
+//! #     ],
+//! #     Params { speed: 0.0 },
+//! # )
+//! # .unwrap();
+//! # let delta_time = 0.1;
+//! state_machine.update(delta_time);
+//! ```
+//!
+//! Update the parameters that are used to determine conditional transitions:
+//!
+//! ```
+//! # use rsanim::*;
+//! # use std::collections::HashMap;
+//!
+//! # #[derive(Clone, Eq, PartialEq, Hash, Debug)]
+//! # enum Animation {
+//! #     Idle,
+//! #     Run,
+//! # }
+//!
+//! # #[derive(Clone, Debug, PartialEq)]
+//! # struct Params {
+//! #     pub speed: f32,
+//! # }
+//!
+//! # let mut state_machine = StateMachine::new(
+//! #     Animation::Idle,
+//! #     HashMap::from([
+//! #         (
+//! #             Animation::Idle,
+//! #             State {
+//! #                 duration: 0.5,
+//! #                 repeat: true,
+//! #             },
+//! #         ),
+//! #         (
+//! #             Animation::Run,
+//! #             State {
+//! #                 duration: 1.0,
+//! #                 repeat: true,
+//! #             },
+//! #         ),
+//! #     ]),
+//! #     vec![
+//! #         Transition {
+//! #             start_state: TransitionStartState::Node(Animation::Idle),
+//! #             end_state: TransitionEndState::Node(Animation::Run),
+//! #             trigger: Trigger::Condition(Box::new(|x: &Params| x.speed > 0.0)),
+//! #         },
+//! #         Transition {
+//! #             start_state: TransitionStartState::Node(Animation::Run),
+//! #             end_state: TransitionEndState::Node(Animation::Idle),
+//! #             trigger: Trigger::Condition(Box::new(|x: &Params| x.speed <= 0.0)),
+//! #         },
+//! #     ],
+//! #     Params { speed: 0.0 },
+//! # )
+//! # .unwrap();
+//! state_machine.update_parameters(&|x| {
+//!     x.speed = 1.0;
+//! });
+//! ```
+
 use std::collections::HashMap;
 use std::fmt::{Debug, Formatter};
 use std::hash::Hash;
